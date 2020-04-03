4796da55728fc698719ca7ace2c3fca50ccfd722
Abdelkader EL Mehdi <fl.bm.mehdi@gmail.com>
2020-04-03 13:45:25 +0200

    added starting component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './login/register/register.component';
+import { MainComponent } from './main/main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
