8ac5368c62f1c90708dcbf441ad7e673bd38a03e
Abdelkader EL Mehdi <fl.bm.mehdi@gmail.com>
2020-04-03 20:39:26 +0200

    added route and redirection, need guard now

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from '../components/main/main/main.component';
+import { LoginComponent } from '../components/login/login/login.component';
+import { RegisterComponent } from '../components/login/register/register.component';
+import { NotFoundComponent } from '../components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'main', redirectTo : '' },
+  { path: 'home', redirectTo: ''},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component : RegisterComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
