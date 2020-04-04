025650b6de7f44e4f72a80949a21ee5417f33d9e
Abdelkader EL Mehdi <fl.bm.mehdi@gmail.com>
2020-04-04 19:24:04 +0200

    finished toolbar for logged and not logged with animation

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, animate, transition,state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('up', style({
+        transform: 'translateY(-80px)',
+      })),
+      state('down', style({
+        transform: 'translateY(0)'
+      })),
+      transition('up => down', [
+        animate('500ms ease')
+      ]),
+      transition('down => up', [
+        animate('500ms ease')
+      ]),
+    ]),
+  ],
 })
 export class ToolbarComponent implements OnInit {
-
+  toAnimate='down'; 
+  toRender:boolean=true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  animate(){
+    this.toAnimate = 'up';
+    setTimeout(()=> {this.toRender= !this.toRender, this.toAnimate='down'} ,500);
+    
   }
 
 }
